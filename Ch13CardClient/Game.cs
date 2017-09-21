75aa236859c274a267093614181503f4bf615ce1
JoshuaMa <majun945@gmail.com>
2017-09-21 17:17:21 +0800

    完成13.5

--- Ch13CardClient/Game.cs
+++ Ch13CardClient/Game.cs
@@ -56,8 +56,127 @@ namespace Ch13CardClient
 
         public int PlayGame()
         {
-            // Code to follow
-            return 0;
+            // Only play if players exist.
+            if (players == null)
+            {
+                return -1;
+            }
+            // Deal initial hands.
+            DealHands();
+            // Initialize game vars, including an initial card to place on the table: playCard
+            bool GameWon = false;
+            int currentPlayer;
+            Card playCard = playDeck.GetCard(currentCard++);
+            discardedCards.Add(playCard);
+            // Main game loop, continues until GameWon == true
+            do
+            {
+                // Loop through players in each game round.
+                for (currentPlayer = 0; currentPlayer < players.Length; currentPlayer++)
+                {
+                    // Write out current player, player hand, and the card on the table
+                    WriteLine($"{players[currentPlayer].Name}'s turn.");
+                    WriteLine("Current hand:");
+                    foreach (Card card in players[currentPlayer].PlayHand)
+                    {
+                        WriteLine(card);
+                    }
+                    WriteLine($"Card in play: {playCard}");
+                    // Prompt player to pick up card on table or draw a new one.
+                    bool inputOK = false;
+                    do
+                    {
+                        WriteLine("Press T to take card in play or D to draw:");
+                        string input = ReadLine();
+                        if (input.ToLower() == "t")
+                        {
+                            // Add card from table to player hand.
+                            WriteLine($"Drawn: {playCard}");
+                            // Remove from discarded cards if possible (if deck is reshuffled it won't be there any more)
+                            if (discardedCards.Contains(playCard))
+                            {
+                                discardedCards.Remove(playCard);
+                            }
+                            players[currentPlayer].PlayHand.Add(playCard);
+                            inputOK = true;
+                        }
+                        if (input.ToLower() == "d")
+                        {
+                            // Add new card from deck to player hand.
+                            Card newCard;
+                            // Only add card if it isn't already in a player hand or in the discard pile
+                            bool cardIsAvailable;
+                            do
+                            {
+                                newCard = playDeck.GetCard(currentCard++);
+                                // Check if card is in discard pile
+                                cardIsAvailable = !discardedCards.Contains(newCard);
+                                if (cardIsAvailable)
+                                {
+                                    // Loop through all player hands to see if newCard is already in hand.
+                                    foreach (Player testPlayer in players)
+                                    {
+                                        if (testPlayer.PlayHand.Contains(newCard))
+                                        {
+                                            cardIsAvailable = false;
+                                            break;
+                                        }
+                                    }
+                                }
+                            } while (!cardIsAvailable);
+                            // Add the card found to player hand.
+                            WriteLine($"Drawn: {newCard}");
+                            players[currentPlayer].PlayHand.Add(newCard);
+                            inputOK = true;
+                        }
+                    } while (!inputOK);
+                    // Display new hand with cards numbered.
+                    WriteLine("New hand:");
+                    for (int i = 0; i < players[currentPlayer].PlayHand.Count; i++)
+                    {
+                        WriteLine($"{i+1}: {players[currentPlayer].PlayHand[i]}");
+                    }
+                    // Prompt player for a card to discard.
+                    inputOK = false;
+                    int choice = -1;
+                    do
+                    {
+                        WriteLine("Choose card to discard:");
+                        string input = ReadLine();
+                        try
+                        {
+                            // Attempt to convert input into a valid card number.
+                            choice = Convert.ToInt32(input);
+                            if (choice > 0 && choice <= 8)
+                            {
+                                inputOK = true;
+                            }
+                        }
+                        catch
+                        {
+                            // Ignore failed conversions, just continue prompting.
+                        }
+                    } while (!inputOK);
+                    /*
+                     * Place reference to removed card in playCard (place the card on the table), 
+                     * then remove card from player hand and add to discarded card pile.
+                     */
+                    playCard = players[currentPlayer].PlayHand[choice - 1];
+                    players[currentPlayer].PlayHand.RemoveAt(choice - 1);
+                    discardedCards.Add(playCard);
+                    WriteLine($"Discarding: {playCard}");
+                    // Space out text for players
+                    WriteLine();
+                    // Check to see if player has won the game, and exit the player loop if so.
+                    GameWon = players[currentPlayer].HasWon();
+                    if (!GameWon)
+                    {
+                        break;
+                    }
+                }
+            } while (!GameWon);
+            // End game, nothing the winning player.
+            return currentPlayer;
         }
     }
 }
